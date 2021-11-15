1d4a1d439f29d356946066031e861d43d444a400
Daria H <daryagavr20@gmail.com>
2021-11-15 21:37:59 +0200

    'minor changes'

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 const TelegramApi = require('node-telegram-bot-api');
-const {keyboardOptions} = require('./options.js')
+const {keyboardOptions} = require('./options.js');
 
-const token = '2019159605:AAFyEFYJU7bR8OvvBEi32aqGE3Hj2ASzasI'
+const token = '2019159605:AAFyEFYJU7bR8OvvBEi32aqGE3Hj2ASzasI';
 
-const bot = new TelegramApi(token, {polling: true})
+const bot = new TelegramApi(token, {polling: true});
 
+const tasks = {}
 
-const startBot = async (chatId) => {
-    await bot.sendMessage(chatId, 'What pills do you take? (enter one name)', keyboardOptions)
+const getData = async (chatId) => {
+    await bot.sendMessage(chatId, 'Enter the description of the task you want to be reminded off (e.g. call Bob, take meds, buy lettuce)', {
+        reply_markup: {
+            force_reply: true
+        }
+    })
+    bot.on('message', async msg => {
+        const task = msg.text;
+        const chatId = msg.chat.id;
+        tasks[chatId] = task;
+        await bot.sendMessage(chatId, 'In how many hours do you need this reminder?', keyboardOptions)
+        bot.on('callback_query', async msg => {
+            const data = msg.data;
+            tasks['timer'] = data
+            console.log(data)
+            console.log(tasks)
+        })
+        
+        console.log(task);
+        console.log(tasks);
+    })
 }
 
+// Starting bots and defining what commands do
 const start = () => {
     bot.setMyCommands([
-        {command: '/start', description: 'Start the bot'},
-        {command: '/set', description: 'Set timer'}
+        {command: '/start', description: 'Start the bot'}
     ])
     
     bot.on('message', async msg => {
         const text = msg.text;
         const chatId = msg.chat.id;
-        console.log(text)
     
+        // Action for start command
         if (text === '/start') {
-            return startBot(chatId);
+            return getData(chatId);
         }
-        return bot.sendMessage(chatId, 'I dont understand you')
     })
-    bot.on('callback_query', async msg => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-    })
+        //bot.on('callback_query', async msg => {
+        //const data = msg.data;
+        //const chatId = msg.message.chat.id;
+    //})
 }
 
-start()
+start();
--- options.js
+++ options.js
@@ -2,7 +2,7 @@ module.exports = {
     keyboardOptions: {
         reply_markup: JSON.stringify({
             inline_keyboard: [
-                [{text: 'Type pill name', callback_data: '1'}],
+                [{text: '1 hour', callback_data: '1'}, {text: '2 hours', callback_data: '2'}, {text: '3 hous', callback_data: '3'}],
             ]
         })
     },
