88a5e747fa61a4a8f05c87244e1c648367deaaba
Daria H <daryagavr20@gmail.com>
2021-11-18 20:00:02 +0200

    minor progress

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const TelegramApi = require('node-telegram-bot-api');
 const {keyboardOptions} = require('./options.js');
-const {token} = require('./config')
+const {token, DB_URL} = require('./config')
 const mongoose = require('mongoose')
 const Task = require('./models/Task')
-const DB_URL = 'mongodb+srv://admin:123@cluster0.ez8ti.mongodb.net/tasks?retryWrites=true&w=majority'
 
 
 const bot = new TelegramApi(token, {polling: true});
@@ -18,15 +17,19 @@ const getData = async (chatId) => {
     bot.on('message', async msg => {
         const task = msg.text;
         const chatId = msg.chat.id;
-        await bot.sendMessage(chatId, 'In how many hours do you need this reminder?', keyboardOptions)
+        await bot.sendMessage(chatId, 'When do you need this reminder? Enter time in the format: HH:MM', keyboardOptions)
         bot.on('callback_query', async msg => {
             const timer = msg.data;
-
-            const newtask = new Task({chatId: chatId, task: task, timer: timer})
+            const date = new Date()
+            console.log(date)
+            // save chatid, task and time to db
+            const newtask = new Task({chatId: chatId, task: task, timer: timer, date: date})
             await newtask.save()
         })
     })
 }
+
+
 
 // Starting bots and defining what commands do
 const start = () => {
--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('mongoose')
 const Task = new Schema ({
     chatId: {type: Number, required: true},
     task: {type: String, required: true},
-    timer: {type: Number, required: true}
+    timer: {type: Number, required: true},
+    date: {type: Date, required: true}
 })
 
 module.exports = model('Task', Task)
